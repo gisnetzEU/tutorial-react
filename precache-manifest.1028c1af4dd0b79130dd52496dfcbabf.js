078e6d41dc77f8c27089f2af7581c3bed0547d38
gisnetz <gisnetz@msn.com>
2020-07-06 17:45:53 +0200

    Updates

---
+++ precache-manifest.1028c1af4dd0b79130dd52496dfcbabf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66fd78a1e2bbdf38626a87438adbe0fd",
+    "url": "/tutorial-react/index.html"
+  },
+  {
+    "revision": "fce020c846d455adc1fe",
+    "url": "/tutorial-react/static/js/2.ca861a05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tutorial-react/static/js/2.ca861a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae4ce9baa6693e63923",
+    "url": "/tutorial-react/static/js/main.e22f5a87.chunk.js"
+  },
+  {
+    "revision": "682edddca3f6012c4544",
+    "url": "/tutorial-react/static/js/runtime-main.4b9bd8d2.js"
+  }
+]);
