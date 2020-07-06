43996f6a70cd48ed341afc965c651adc39324c9b
gisnetz <gisnetz@msn.com>
2020-07-06 18:55:03 +0200

    Updates

---
+++ precache-manifest.1f9e29362b3cb191c675ea995730315f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4132e6a445d27d060620fa2ede1f445a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fce020c846d455adc1fe",
+    "url": "./static/js/2.ca861a05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ca861a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae4ce9baa6693e63923",
+    "url": "./static/js/main.e22f5a87.chunk.js"
+  },
+  {
+    "revision": "1ab28bad44dd9355558a",
+    "url": "./static/js/runtime-main.f59ff679.js"
+  }
+]);
